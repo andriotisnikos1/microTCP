c25e0e86d7369a64258a3ea0fbe6896db8445c91
Νίκος Αποστολίδης <nikosapostolidis@MacBook-Pro-Nikos.local>
2025-12-15 20:24:17 +0200

    added server_microtcp

--- test/bandwidth_test.c
+++ test/bandwidth_test.c
@@ -156,10 +156,98 @@ server_tcp (uint16_t listen_port, const char *file)
   return 0;
 }
 
-int
-server_microtcp (uint16_t listen_port, const char *file)
-{
-  /*TODO: Write your code here */
+int server_microtcp (uint16_t listen_port, const char *file) {
+  
+  uint8_t *buffer;
+  FILE *fp;
+  microtcp_sock_t sock;
+  int accepted;
+  int received;
+  ssize_t written;
+  ssize_t total_bytes = 0;
+  socklen_t client_addr_len;
+
+  struct sockaddr_in sin;
+  struct sockaddr client_addr;
+  struct timespec start_time;
+  struct timespec end_time;
+
+  /* Allocate memory for the application receive buffer */
+  buffer = (uint8_t *) malloc (CHUNK_SIZE);
+  if (!buffer) {
+    perror ("Allocate application receive buffer");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Open the file for writing the data from the network */
+  fp = fopen (file, "w");
+  if (!fp) {
+    perror ("Open file for writing");
+    free (buffer);
+    exit(EXIT_FAILURE);
+  }
+
+  sock = microtcp_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sock.state == INVALID) {
+    perror ("Opening microTCP socket");
+    free (buffer);
+    fclose (fp);
+    exit(EXIT_FAILURE);
+  }
+
+  memset (&sin, 0, sizeof(struct sockaddr_in));
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons (listen_port);
+  /* Bind to all available network interfaces */
+  sin.sin_addr.s_addr = INADDR_ANY;
+
+  if (microtcp_bind (&sock, (struct sockaddr *) &sin, sizeof(struct sockaddr_in)) == -1) {
+    perror ("microTCP bind");
+    free (buffer);
+    fclose (fp);
+    exit(EXIT_FAILURE);
+  }
+
+  client_addr_len = sizeof(struct sockaddr);
+  accepted = microtcp_accept(&sock, &client_addr, client_addr_len);
+  if (accepted < 0) {
+    perror ("microTCP accept");
+    free (buffer);
+    fclose (fp);
+    exit(EXIT_FAILURE);
+  }
+
+  /*
+   * Start processing the received data.
+   *
+   * Also start measuring time. Not the most accurate measurement, but
+   * it is a good starting point.
+   *
+   * At hy-435 we deal with bandwidth measurements software in a more
+   * right and careful way :-)
+   */
+
+   sock.peer_address = sin;
+   sock.peer_address_len = sizeof(struct sockaddr_in);
+  clock_gettime (CLOCK_MONOTONIC_RAW, &start_time);
+  received = microtcp_recv ( &sock, buffer, CHUNK_SIZE, 0);
+  while (received > 0) {
+    written = fwrite (buffer, sizeof(uint8_t), received, fp);
+    total_bytes += received;
+    if (written * sizeof(uint8_t) != received) {
+      printf ("Failed to write to the file the"
+              " amount of data received from the network.\n");
+      free (buffer);
+      fclose (fp);
+      exit(EXIT_FAILURE);
+    }
+  }
+  clock_gettime (CLOCK_MONOTONIC_RAW, &end_time);
+  print_statistics (total_bytes, start_time, end_time);
+
+  fclose (fp);
+  free (buffer);
+
   return 0;
 }
 
